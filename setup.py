e142bd6ff56c98cd0356f45a02c2e9a7b0d15a5b
Daniel Ceglinski <danielceglinski@web.de>
2018-01-31 14:14:30 +0100

    update new name in setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ with open('README.rst', 'r') as f:
     long_description = f.read()
 
 setup(
-    name='headline',
+    name='holiness',
     version='0.1',
     author='Daniel Ceglinski',
     author_email='danielceglinski@web.de',
     packages=find_packages(exclude=['docs']),
-    url='https://github.com/MadaoG/headline',
+    url='https://github.com/MadaoG/holiness',
     license='MIT',
     description='Produces nice headlines',
     long_description=long_description,
