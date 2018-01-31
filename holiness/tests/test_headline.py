b19444011859f376017b6349eb8f64e358e48c94
Daniel Ceglinski <danielceglinski@web.de>
2018-01-31 14:24:05 +0100

    rename headline to holiness in test file

--- holiness/tests/test_headline.py
+++ holiness/tests/test_headline.py
@@ -1,67 +1,67 @@
 from unittest import TestCase
 
-import headline
+import holiness
 
 class TestHeadline(TestCase):
     def test_basic_headline(self):
-        h = headline.headline("headline")
+        h = holiness.headline("headline")
         self.assertTrue(h == "==========================  H E A D L I N E  ===========================")
         
     def test_headline_width(self):
-        h = headline.headline("headline", width=42)
+        h = holiness.headline("headline", width=42)
         self.assertTrue(h == "===========  H E A D L I N E  ============")
         
     def test_headline_uppercase(self):
-        h = headline.headline("headline", width=42, uppercase=False)
+        h = holiness.headline("headline", width=42, uppercase=False)
         self.assertTrue(h == "===========  h e a d l i n e  ============")
         
     def test_headline_spaces(self):
-        h = headline.headline("headline", width=42, nr_spaces=0)
+        h = holiness.headline("headline", width=42, nr_spaces=0)
         self.assertTrue(h == "=============H E A D L I N E==============")
         
     def test_headline_spaces_max(self):
         """too much nr_spaces should not be ignored"""
-        h = headline.headline("headline", width=42, nr_spaces=20)
+        h = holiness.headline("headline", width=42, nr_spaces=20)
         self.assertTrue(h == "=                    H E A D L I N E                    =")
         
     def test_headline_sym(self):
-        h = headline.headline("headline", width=42, border="#")
+        h = holiness.headline("headline", width=42, border="#")
         self.assertTrue(h == "#==========  H E A D L I N E  ===========#")
         
     def test_headline_sym_pair_tuple(self):
-        h = headline.headline("headline", width=42, border=(">>", "<<"))
+        h = holiness.headline("headline", width=42, border=(">>", "<<"))
         self.assertTrue(h == ">>=========  H E A D L I N E  ==========<<")
         
     def test_headline_sym_pair_list(self):
-        h = headline.headline("headline", width=42, border=["/*", "*/"])
+        h = holiness.headline("headline", width=42, border=["/*", "*/"])
         self.assertTrue(h == "/*=========  H E A D L I N E  ==========*/")
         
     def test_headline_sym_mirror(self):
-        h = headline.headline("headline", width=42, border="# ")
+        h = holiness.headline("headline", width=42, border="# ")
         self.assertTrue(h == "# =========  H E A D L I N E  ========== #")
         
     def test_headline_like_c(self):
-        h = headline.headline("headline", width=42, border="/*", char="*")
+        h = holiness.headline("headline", width=42, border="/*", char="*")
         self.assertTrue(h == "/**********  H E A D L I N E  ***********/")
         
     def test_headline_char(self):
-        h = headline.headline("headline", width=42, char="-")
+        h = holiness.headline("headline", width=42, char="-")
         self.assertTrue(h == "-----------  H E A D L I N E  ------------")
         
     def test_headline_char_sym(self):
-        h = headline.headline("headline", width=42, char="-", border="!")
+        h = holiness.headline("headline", width=42, char="-", border="!")
         self.assertTrue(h == "!----------  H E A D L I N E  -----------!")
         
     def test_headline_char_sym_equal(self):
-        h = headline.headline("headline", width=42, char="-", border="=")
+        h = holiness.headline("headline", width=42, char="-", border="=")
         self.assertTrue(h == "=----------  H E A D L I N E  -----------=")
         
     def test_headline_spacesym(self):
-        h = headline.headline("headline", width=42, spacesym="_")
+        h = holiness.headline("headline", width=42, spacesym="_")
         self.assertTrue(h == "===========__H_E_A_D_L_I_N_E__============")
         
     def test_headline_surround(self):
-        h = headline.headline("headline", width=42, surround=True)
+        h = holiness.headline("headline", width=42, surround=True)
         self.assertTrue(h == 
                 "==========================================\n" +
                 "===========  H E A D L I N E  ============\n" +
@@ -69,7 +69,7 @@ class TestHeadline(TestCase):
                 )
         
     def test_headline_all(self):
-        h = headline.headline(
+        h = holiness.headline(
             "headline",
             width=42,
             surround=True,
@@ -86,5 +86,5 @@ class TestHeadline(TestCase):
                 "% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ %")
 
     def test_headline_char_multiple(self):
-        h = headline.headline("headline", width=42, char="+-")
+        h = holiness.headline("headline", width=42, char="+-")
         self.assertTrue(h == "+-+-+-+-+-+  H E A D L I N E  +-+-+-+-+-+-")
